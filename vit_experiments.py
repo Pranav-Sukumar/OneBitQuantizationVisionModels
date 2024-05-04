d6b2cc201b9f0148d916ca110bd17bbfc5f45bae
Pranav Sukumar <pranavsukumar@pranav21.mynetworksettings.com>
2024-05-04 11:26:38 -0400

    fixed training loop for vit

--- vit_experiments.py
+++ vit_experiments.py
@@ -14,7 +14,6 @@ from models.vit import VITModelNotQuantized
 from quantization_utils.bit_linear_custom import BitLinear
 
 from quantization_utils.quantization_functions import QuantizationUtilityFunctions
-from train_test_utils.train_test_functions import TrainTestUtils
 
 
 import wandb
@@ -57,18 +56,100 @@ testset_cifar_100 = torchvision.datasets.CIFAR100(root='./data', train=False,
 testloader_cifar_100 = torch.utils.data.DataLoader(testset_cifar_100, batch_size=32,
                                          shuffle=False, num_workers=2)
 
-  
+def train(device, model: nn.Module, dataloader: DataLoader, experiment_name, num_epochs = 5):
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+    for epoch in range(num_epochs):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+        total_batch_loss = 0.0
+        num_batches = 0
+
+        for i, data in enumerate(dataloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+            num_batches += 1
+
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = model(inputs)
+            loss = criterion(outputs.logits, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            total_batch_loss += loss.item()
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                    (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+        average_batch_loss = total_batch_loss / num_batches
+        print(f"Epoch {epoch} has a loss of {average_batch_loss}")
+
+def test(device, model: nn.Module, dataloader: DataLoader, max_samples=None) -> float:
+    correct = 0
+    total = 0
+    n_inferences = 0
+
+    with torch.no_grad():
+        for data in dataloader:
+            images, labels = data
+
+            images = images.to(device)
+            labels = labels.to(device)
+
+            outputs = model(images)
+            _, predicted = torch.max(outputs.logits, 1)  # Access the 'logits' attribute instead of 'data'
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+            if max_samples:
+                n_inferences += images.shape[0]
+                if n_inferences > max_samples:
+                    break
+
+    return 100 * correct / total
+
+def test_and_export_logs(device, wandb_log_name, model_to_test, data_loader):
+        wandb.init(
+        # Set the project where this run will be logged
+        project="OneBitQuantization",
+        # We pass a run name (otherwise it’ll be randomly assigned, like sunshine-lollypop-10)
+        name=wandb_log_name,
+        # Track hyperparameters and run metadata
+        )
+        s = time.time()
+        for i in range(30):
+            score = test(device = device, model = model_to_test, dataloader = data_loader)
+        average_inference_time = (time.time() - s) / 30
+        print(average_inference_time)
+        print('Accuracy of the network on the test images: {}%'.format(score))
+
+        wandb.log({"Test Accuracy": score})
+        wandb.log({"Average Inference Time": average_inference_time})
+        
+        #print(f"Size of model is {print_model_size(model_to_test)}")
+
+        wandb.finish()
 
 print("Training vit")
 
 vit = VITModelNotQuantized.vit_model.to(device)
-TrainTestUtils.train(device, vit, trainloader_cifar_10, "vit-CIFAR-10-NoQuantization", 5)
-TrainTestUtils.test_and_export_logs(device = device, wandb_log_name = "vit-CIFAR-10-NoQuantization", model_to_test = vit, data_loader = testloader_cifar_10)
+train(device, vit, trainloader_cifar_10, "vit-CIFAR-10-NoQuantization", 5)
+test_and_export_logs(device = device, wandb_log_name = "vit-CIFAR-10-NoQuantization", model_to_test = vit, data_loader = testloader_cifar_10)
 
 print("post quantization training for vit")
 vit_quantized_linear = QuantizationUtilityFunctions.copy_model(vit)
 QuantizationUtilityFunctions.quantize_layer_weights(device, vit_quantized_linear)
-TrainTestUtils.test_and_export_logs(device, "vit-CIFAR-10-PostTrainingQuantizationLinear", vit_quantized_linear, testloader_cifar_10)
+test_and_export_logs(device, "vit-CIFAR-10-PostTrainingQuantizationLinear", vit_quantized_linear, testloader_cifar_10)
 
 print("quantization aware training for vit")
 vit_quantized_aware = VITModelNotQuantized.vit_model.to(device)
@@ -84,7 +165,7 @@ def replace_linear_layers(module):
 # Apply the function to the entire model
 replace_linear_layers(vit_quantized_aware)
 vit_quantized_aware.to(device)
-TrainTestUtils.train(device, vit, trainloader_cifar_10, "vit-CIFAR-10-QuantizationAwareLinear", 5)
-TrainTestUtils.test_and_export_logs(device = device, wandb_log_name = "vit-CIFAR-10-QuantizationAwareLinear", model_to_test = vit_quantized_aware, data_loader = testloader_cifar_10)
+train(device, vit, trainloader_cifar_10, "vit-CIFAR-10-QuantizationAwareLinear", 5)
+test_and_export_logs(device = device, wandb_log_name = "vit-CIFAR-10-QuantizationAwareLinear", model_to_test = vit_quantized_aware, data_loader = testloader_cifar_10)
 
 
